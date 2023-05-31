d0f0b601b94bc3eac6cc15b7f7dc5f57979a2160
nlwmode <nlwmode@gmail.com>
2023-05-31 16:22:16 +0800

    remove include

--- include/operations/optimization/refactor.hpp
+++ include/operations/optimization/refactor.hpp
@@ -11,7 +11,6 @@
 #include "algorithms/cleanup.hpp"
 #include "algorithms/ref_deref.hpp"
 #include "database/views/depth_view.hpp"
-#include "detail/node_resynthesis.hpp"
 #include "detail/sop_refactoring.hpp"
 #include "kitty/constructors.hpp"
 #include "kitty/isop.hpp"
